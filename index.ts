2b7cb983802c39c295af468e53987367f6549d5b
Spencer Jonas <spencerjonas@gmail.com>
2023-12-27 00:27:57 -0500

    Implemented style selection - ugly code and u-turn/abilities don't work yet

--- index.ts
+++ index.ts
@@ -3,15 +3,18 @@ import { GenerationNum } from "@pkmn/types";
 import { Generations, Data, Species } from "@pkmn/data";
 import { Dex } from "@pkmn/dex";
 import { Nonstandard } from "@pkmn/types";
-import { createReadStream, writeFile, readFileSync } from "fs";
+import { createReadStream, createWriteStream, writeFile, readFileSync, WriteStream } from "fs";
 import { json2csv } from "json-2-csv";
-import { parse } from "csv-parse";
 
 import * as path from "node:path";
 
 import format from './formatConfig.json' assert { type: "json" };
 
 const dataDir = "./data";
+const filepath = path.resolve("moves.csv");
+const fileContent = readFileSync(filepath, { encoding: 'utf-8' });
+const moves = fileContent.split(',');
+
 
 const NATDEX_UNOBTAINABLE_SPECIES = [
   'Eevee-Starter', 'Floette-Eternal', 'Pichu-Spiky-eared', 'Pikachu-Belle', 'Pikachu-Cosplay',
@@ -29,23 +32,14 @@ const NATDEX_EXISTS = (d: Data) => {
 };
 
 var gens = new Generations(Dex);
+var natdexLabel = "";
 if (format.natdex == true) {
     gens = new Generations(Dex, NATDEX_EXISTS);
+    natdexLabel = "ND";
 }
 
 
-type PokemonLearnsMoveRow = {
-  pokemonPsId: string;
-  movePsId: string;
-};
-
-const filepath = path.resolve("moves.csv");
-const fileContent = readFileSync(filepath, { encoding: 'utf-8' });
-const moves = fileContent.split(',');
-
 async function writeLearnsetCsv() {
-  const learnsetRows: PokemonLearnsMoveRow[] = [];
-
   const learnMethodByPokemonPsId_MovePsId = new Map<string, Set<string>>();
 
   const gen = gens.get(format.gen);
@@ -91,29 +85,117 @@ async function writeLearnsetCsv() {
   }
 
   
+  if (format.technoStyle == true) {
+    type PokemonLearnsMoveRow = {
+      pokemonPsId: string;
+      movePsId: string;
+    };
+    const learnsetRows: PokemonLearnsMoveRow[] = [];
 
-  for (let [key, value] of learnMethodByPokemonPsId_MovePsId.entries()) {
-    const [genString, pokemonName, moveId] = key.split("_");
-
-    if (!genString || !pokemonName || !moveId) {
-      continue;
+    for (let [key, value] of learnMethodByPokemonPsId_MovePsId.entries()) {
+      const [genString, pokemonName, moveId] = key.split("_");
+  
+      if (!genString || !pokemonName || !moveId) {
+        continue;
+      }
+  
+      learnsetRows.push({
+        pokemonPsId: pokemonName,
+        movePsId: moveId,
+      });
     }
 
-    learnsetRows.push({
-      pokemonPsId: pokemonName,
-      movePsId: moveId,
+    writeFile(
+      path.join(dataDir, "gen" + format.gen.toString() + natdexLabel + "learnset") + ".csv",
+      json2csv(learnsetRows, { emptyFieldValue: "" }),
+      console.error
+    );
+  }
+  else {
+    /*const csvWriter = createArrayCsvWriter({
+      header: moves,
+      path: path.join(dataDir, "gen" + format.gen.toString() + natdexLabel + "learnset") + ".csv"
+    });*/
+
+    const n = 700; 
+    const monmoves: String[][] = new Array(n)
+                                   .fill("")
+                                   .map(() => 
+                                     new Array(n).fill("")
+                                   );
+    var count = 0
+    for (const move of moves) {
+      count = 0;
+      for (let [key, value] of learnMethodByPokemonPsId_MovePsId.entries()) {
+        const [genString, pokemonName, moveId] = key.split("_");
+    
+        if (!genString || !pokemonName || !moveId) {
+          continue;
+        }
+        if (move == moveId) {
+          console.log(pokemonName + " - " + moveId);
+          
+          monmoves[count]![moves.indexOf(move)] = pokemonName;
+          count++;
+        }
+        
+      }
+  
+    }
+    /*for (let [key, value] of learnMethodByPokemonPsId_MovePsId.entries()) {
+      const [genString, pokemonName, moveId] = key.split("_");
+  
+      if (!genString || !pokemonName || !moveId) {
+        continue;
+      }
+      records.push(["a"])
+    }*/
+
+    /*csvWriter.writeRecords(monmoves)
+    .then(() => {
+        console.log('...Done');
+    });*/
+    /*writeFile(
+      path.join(dataDir, "gen" + format.gen.toString() + natdexLabel + "learnset") + ".csv",
+      json2csv(monmoves, { emptyFieldValue: "" }),
+      console.error
+    );*/
+    const writeUsers = createWriteStream(path.join(dataDir, "gen" + format.gen.toString() + natdexLabel + "learnset") + ".csv");
+    const header = moves.join(",") + "\n";
+    writeUsers.write(header, 'utf8');
+
+    writeTenMillionUsers(monmoves, writeUsers, 'utf-8', () => {
+      writeUsers.end();
     });
   }
 
-  var natdexLabel = ""
-  if (format.natdex == true) {
-    natdexLabel = "ND";
+  
+}
+
+function writeTenMillionUsers(monmoves: String[][], writer: WriteStream, encoding: BufferEncoding, callback: () => void) {
+  let i = 500;
+  let id = 0;
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      const data = monmoves[id]?.join(",") + "\n";
+      id += 1;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+// see if we should continue, or wait
+// don't pass the callback, because we're not done yet.
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+// had to stop early!
+// write some more once it drains
+      writer.once('drain', write);
+    }
   }
-  writeFile(
-    path.join(dataDir, "gen" + format.gen.toString() + natdexLabel + "learnset") + ".csv",
-    json2csv(learnsetRows, { emptyFieldValue: "" }),
-    console.error
-  );
+write()
 }
 
 writeLearnsetCsv();
